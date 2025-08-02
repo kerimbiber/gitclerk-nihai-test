8ef4165e25031152f8431689400043ed5c4eb41b
Kerim Biber <kerimbiber@gmail.com>
2025-08-02 23:24:16 +0300

    feat: Toplama fonksiyonu eklendi ve örnek kullanım eklendi

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ def selamla(isim):
     # Fonksiyon artık bir değer döndürüyor
     return f"Merhaba, {isim}!"
 
+def topla(a, b):
+    return a + b
+
 mesaj = selamla("Yapay Zeka")
 print(mesaj)
+
+sonuc = topla(5, 10)
+print(f"5 + 10 = {sonuc}")
